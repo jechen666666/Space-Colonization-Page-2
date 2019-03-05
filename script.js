036af9c9174b4265c3b2b12e9fe0cef6e574267b
jechen666666 <43358343+jechen666666@users.noreply.github.com>
2019-03-04 16:05:33 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".end").hide();
+
+$(".text").click (function() {
+    $("body").css("background","none");
+    $("body").css("background-color","black");
+    $(".end").show();
+    $(".start").hide();
+    $(".dialogue").hide();
+});
+
+
+
